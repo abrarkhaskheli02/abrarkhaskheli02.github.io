6b06977c8968b1cb4f9301e44955844d841f5771
abrarkhaskheli02 <abrarkhaskheli02@gmail.com>
2026-09-08 12:48:06 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,18 @@
-const CACHE_NAME = "abrar-portal-v1";
-‎
-‎const FILES = [
-‎  "./",
-‎  "./index.html",
-‎  "./manifest.json",
-‎  "./sw.js",
-‎  "./icon.svg"
-‎];
-‎
-‎self.addEventListener("install", event => {
-‎  event.waitUntil(
-‎    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-‎  );
-‎  self.skipWaiting();
-‎});
-‎
-‎self.addEventListener("activate", event => {
-‎  event.waitUntil(self.clients.claim());
-‎});
-‎
-‎self.addEventListener("fetch", event => {
-‎  event.respondWith(
-‎    caches.match(event.request).then(response => {
-‎      return response || fetch(event.request);
-‎    })
-‎  );
-‎});
-‎
+const CACHE='ak-portal-v8-offline';
+const ASSETS=["./index.html", "./whatsapp.png", "./facebook.png", "./manifest.webmanifest"];
+self.addEventListener('install',event=>event.waitUntil(
+  caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
+));
+self.addEventListener('activate',event=>event.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached || fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
